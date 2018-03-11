7cfd91badc18fd1a72226e0fdca4518620415279
Gu Junhua <jhgu@nao.cas.cn>
2018-03-11 22:53:28 +0800

    新文件：   src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+extern crate chrono;
+extern crate num_traits;
+
+pub mod julian_day;
+pub mod sidereal;
+pub mod precession;
+pub mod eqpoint;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
